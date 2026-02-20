a9657f7873f8015f729cfa1c547ac80a5266c139
Jaoreir <jaoreir@gmail.com>
2026-02-20 16:45:59 +0800

    fix(armor): Missing "hat" part definition

--- common/src/main/java/net/satisfy/candlelight/client/model/SuitLeggingsModel.java
+++ common/src/main/java/net/satisfy/candlelight/client/model/SuitLeggingsModel.java
@@ -32,7 +32,7 @@ public class SuitLeggingsModel<T extends LivingEntity> extends HumanoidModel<T> 
         PartDefinition root = meshDefinition.getRoot();
 
         PartDefinition head = root.addOrReplaceChild("head", CubeListBuilder.create(), PartPose.ZERO);
-        head.addOrReplaceChild("hat", CubeListBuilder.create(), PartPose.ZERO);
+        root.addOrReplaceChild("hat", CubeListBuilder.create(), PartPose.ZERO);
 
         root.addOrReplaceChild("right_arm", CubeListBuilder.create(), PartPose.offset(-5.0F, 2.0F, 0.0F));
         root.addOrReplaceChild("left_arm", CubeListBuilder.create(), PartPose.offset(5.0F, 2.0F, 0.0F));
